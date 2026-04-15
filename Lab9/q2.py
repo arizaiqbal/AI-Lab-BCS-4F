1cc6973d4995bdc0e104d18f700771266c3cfe54
Ariza Iqbal <areezashaikh23@gmail.com>
2026-04-15 22:40:01 +0500

    Update q2.py

--- Lab9/q2.py
+++ Lab9/q2.py
@@ -1,3 +1,33 @@
+!pip install pgmpy
+from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+
+# Step 1: Define structure
+model = DiscreteBayesianNetwork([
+    ('I', 'G'),
+    ('S', 'G'),
+    ('D', 'G'),
+    ('G', 'P')
+])
+
+# Step 2: Define CPDs
+
+cpd_I = TabularCPD('I', 2, [[0.7], [0.3]])
+cpd_S = TabularCPD('S', 2, [[0.6], [0.4]])
+cpd_D = TabularCPD('D', 2, [[0.6], [0.4]])
+
+cpd_G = TabularCPD(
+    variable='G', variable_card=3,
+    values=[
+        [0.7, 0.5, 0.4, 0.2, 0.3, 0.2, 0.1, 0.05],
+        [0.2, 0.3, 0.4, 0.5, 0.4, 0.3, 0.3, 0.25],
+        [0.1, 0.2, 0.2, 0.3, 0.3, 0.5, 0.6, 0.7]
+    ],
+    evidence=['I', 'S', 'D'],
+    evidence_card=[2, 2, 2]
+)
+
 cpd_P = TabularCPD(
     variable='P', variable_card=2,
     values=[
